fb7ceae757120f3085bfdf9b869cfe6c3df3cff6
sonecaz <160507585+sonecaz0@users.noreply.github.com>
2024-08-05 11:41:31 -0300

    Add files via upload

---
+++ revisaophp/teste.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>revisão de php</h1>
+    <a href="index.html">Home</a>
+    <?php
+       
+       echo "<p> esta linha é um php </p>";
+       
+       $revisao="<p> Ola mundo </p>";
+        echo $revisao;
+
+        $x=3;
+        $y=1;
+        $result=$x+$y;
+        echo "O Resultado da soma é: $result </p>";
+
+        echo "<p> A variavel x é do tipo: </p>";
+       
+       ?>
+        
+    
+    
+</body>
+</html>
